64e9f7fc0d36ed387a2e34728bf29ff4f572c81f
Esteban <baab2an@live.fr>
2024-05-30 11:39:43 +0200

    FEAT documentation pour la function exports updateUser

--- functions/api/updateUser.js
+++ functions/api/updateUser.js
@@ -2,6 +2,13 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const cors = require("cors")({ origin: true });
 
+/**
+ * Update a user's idendification data.
+ * @param body {String, Object}
+ * @return {string} message success or error
+ * @forPlay https://us-central1-codersbaban.cloudfunctions.net/updateUser
+ * @documentation https://firebase.google.com/docs/auth/admin/manage-users#update_a_user
+ */
 exports.updateUser = functions.https.onRequest((req, res) => {
   const uid = req.body.uid;
   const data = req.body.data;
